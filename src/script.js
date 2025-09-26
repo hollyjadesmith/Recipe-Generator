c26e1eb5eef7d0ccc5a48805aa4896a31c2fe187
hollyjadesmith <hollysmithfachwen@gmail.com>
2025-09-26 23:58:02 +0100

    remodified the user interface - improved javascript by generating a temporary message and hiding the recipe-text box when no search input

--- src/script.js
+++ src/script.js
@@ -7,26 +7,28 @@ console.log("Recipe generated...");
     .trim();
 
 new Typewriter("#recipe-text", {
-  strings: [recipeText],
   autoStart: true,
   cursor: "",
   loop: false,
   delay: 20,
-  deleteSpeed: null
-});
+})
+    .typeString(recipeText)
+    .start();
 }
 
 function generateRecipe(event){
     event.preventDefault();
 
-    let instructionsInput = document.querySelector("#user-instructions");
+let instructionsInput = document.querySelector("#user-instructions");
 let aipKey = "o94ea318dba23fta40204c3af2064fda";
 
 let prompt = `User instructions are: Generate a short recipe of no more than 10 lines based on the ingredients: ${instructionsInput.value}`;
-let context = "You are a recipe expert and love to write short recipes to help people decide on what to cook based on the contents of their fridge. Your mission is to generate a short recipe in basic HTML and make sure to follow the user instructions inputted.";
+let context = "You are a recipe expert and love to write short recipes to help people decide on what to cook based on the contents of their fridge. Your mission is to generate a short recipe in basic HTML, with h1 as the header in size 16px and p in size 14px for the rest of the text, and make sure to follow the user instructions inputted.";
 let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${aipKey}`;
 
-
+let recipeElement = document.querySelector("#recipe-text");
+recipeElement.classList.remove("hidden");
+recipeElement.innerHTML = `Conjouring up a recipe...`;
 axios.get(apiUrl).then(displayRecipe);
 
 
